a48ea54a21e66efe90a786201411c392038d7d3c
inrgihc <inrgihc@126.com>
2025-08-29 22:51:16 +0800

    version for 1.6.0

--- build-docker/build_and_push_image.sh
+++ build-docker/build_and_push_image.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-SQLREST_VERSION=1.5.0
+SQLREST_VERSION=1.6.0
 BUILD_DOCKER_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 PROJECT_ROOT_DIR=$( dirname "$BUILD_DOCKER_DIR")
 DOCKER_SQLREST_DIR=$BUILD_DOCKER_DIR/sqlrest
